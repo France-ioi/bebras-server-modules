5b8131bc67dd700d11784da468a4ec089bab9f8c
Sebastien Carlier <s.carlier@epixode.fr>
2018-01-28 21:38:52 +0000

    tokens_api: pass itemUrl params to task

--- libs/tokens_api.js
+++ libs/tokens_api.js
@@ -1,37 +1,48 @@
 var fetch = require('isomorphic-fetch')
 var config = require('../config')
 var jwt = require('jsonwebtoken')
+var qs = require('querystring')
+var Url = require('url')
 
-
-function parseTaskId(url) {
-    var params = require('querystring').parse(require('url').parse(url).query)
-    return params.taskID ? params.taskID : null
+function getTaskParams(itemUrl) {
+    return qs.parse(Url.parse(itemUrl).query)
 }
-
+function getTaskID(params) {
+    return params.taskID ? params.taskID : null;
+}
 
 module.exports = {
 
     decodeTask: function(token, callback) {
         var payload = jwt.decode(token)
-        var id = parseTaskId(payload.itemUrl)
-        if(!id) {
-            return callback(new Error('Task token error: taskID missed in itemUrl'))
+        if(!payload) {
+            return callback(new Error('Task token error: unparseable token'))
         }
+        var params = getTaskParams(payload.itemUrl)
+        var task_id = getTaskID(params)
+        if(!task_id) {
+            return callback(new Error('Task token error: taskID missing from itemUrl'))
+        }
+        /* XXX No good reason to parse and check random_seed? */
         var random_seed = parseInt(payload.randomSeed, 10)
         if(!Number.isInteger(random_seed) || random_seed < 0) {
-            return callback(new Error('Task token error: randomSeed missed or incorrect'))
+            return callback(new Error('Task token error: randomSeed missing or incorrect'))
         }
         callback(null, {
-            id,
+            task_id, /* added for for consistency */
+            id: task_id, /* obsolete, kept for compatibility */
             random_seed,
-            hints_requested: payload.sHintsRequested
+            hints_requested: payload.sHintsRequested,
+            params
+            /* Is there a good reason not to pass the full token? */
         })
     },
 
 
     decodeAnswer: function(token, callback) {
         var payload = jwt.decode(token)
-        var task_id = parseTaskId(payload.itemUrl)
+        var params = getTaskParams(payload.itemUrl)
+        var task_id = getTaskID(params)
         if(!task_id) {
             return callback(new Error('Answer token error: taskID missed in itemUrl'))
         }
@@ -43,7 +54,8 @@ module.exports = {
             idUserAnswer: payload.idUserAnswer,
             value: payload.sAnswer,
             task_id,
-            random_seed
+            random_seed,
+            params
         })
     },
 
