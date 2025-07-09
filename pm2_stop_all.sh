f9de8b6b509adde6eec197a699be83ab8a6145ba
Sébastien Tainon <arkandias1@hotmail.fr>
2025-07-09 16:30:16 +0200

    Update PM2 commands

--- pm2_stop_all.sh
+++ pm2_stop_all.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
-pm2 stop bsm-data
 pm2 delete bsm-data
-pm2 stop bsm-tasks
 pm2 delete bsm-tasks
-pm2 stop bsm-assets
 pm2 delete bsm-assets
-pm2 stop bsm-tokens
 pm2 delete bsm-tokens
+pm2 delete bsm-ai
