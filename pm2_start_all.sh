ecc4b399db4de40f10c73a7605a32c671c81545c
Sébastien Tainon <arkandias1@hotmail.fr>
2025-07-23 12:19:35 +0200

    Add quiz handler in pm2_start_all

--- pm2_start_all.sh
+++ pm2_start_all.sh
@@ -4,3 +4,4 @@ pm2 start dist/server.js --name bsm-tasks -- tasks -p=3101
 pm2 start dist/server.js --name bsm-assets -- assets -p=3102
 pm2 start dist/server.js --name bsm-tokens -- tokens -p=3103
 pm2 start dist/server.js --name bsm-ai -- ai -p=3104
+pm2 start dist/server.js --name bsm-quiz -- quiz -p=3105
--- pm2_stop_all.sh
+++ pm2_stop_all.sh
@@ -4,3 +4,4 @@ pm2 delete bsm-tasks
 pm2 delete bsm-assets
 pm2 delete bsm-tokens
 pm2 delete bsm-ai
+pm2 delete bsm-quiz
